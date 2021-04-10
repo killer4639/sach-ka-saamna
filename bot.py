d5457e25c4ffc417f7e852a525b4fd6fbf2dd200
killer4639 <shivagupta4639@gmail.com>
2021-04-10 20:01:50 +0530

    freezed

--- bot.py
+++ bot.py
@@ -5,30 +5,63 @@ from dotenv import load_dotenv
 from flask import Flask
 from slackeventsapi import SlackEventAdapter
 import requests
-env_path=Path('.')/'.env'
+from io import BytesIO
+from PIL import Image, ImageChops, ImageEnhance
+
+env_path = Path(".") / ".env"
 load_dotenv(dotenv_path=env_path)
 
-app=Flask(__name__)
-slack_event_adapter=SlackEventAdapter(os.environ['SIGNING_SECRET'],'/slack/events',app)
-client=slack.WebClient(token=os.environ['SLACK_TOKEN'])
+app = Flask(__name__)
+slack_event_adapter = SlackEventAdapter(
+    os.environ["SIGNING_SECRET"], "/slack/events", app
+)
+client = slack.WebClient(token=os.environ["SLACK_TOKEN"])
 # client.chat_postMessage(channel='#slack-bot1',text="Hello world!")
-BOT_ID=client.api_call("auth.test")['user_id']
+BOT_ID = client.api_call("auth.test")["user_id"]
 
-@slack_event_adapter.on('message')
+
+@slack_event_adapter.on("message")
 def message(payload):
-    event=payload.get('event',{})
-    print(event)
-    channel_id=event.get('channel')
-    user_id=event.get('user')
+    event = payload.get("event", {})
+    # print(event)
+    # print('###########################################################')
+    channel_id = event.get("channel")
+    user_id = event.get("user")
+    if BOT_ID != user_id:
+        text = event.get("text")
+        if text != "":
+            client.chat_postMessage(channel=channel_id, text=text)
+        else:
+            url = event.get("files")[0].get("url_private_download")
+            token = os.environ["SLACK_TOKEN"]
+            response = requests.get(url, headers={"Authorization": "Bearer %s" % token})
+            image = Image.open(BytesIO(response.content)).convert("RGB")
+
+            # if you want to locally import the image from path then comment above line
+            # Uncomment the below line
+            # img = Image.open(path).convert('RGB')
+
+            # saving local
+            image.save("temp_filename.jpg")
+            client.chat_postMessage(
+                {
+                    "channel": channel_id,
+                    "attachments": [
+                        {
+                            "accessory": {
+                                "type": "image",
+                                "image_url": url,
+                                "alt_text": "Haunted hotel image",
+                            }
+                        }
+                    ],
+                }
+            )
+            print(url)
     # if BOT_ID!=user_id:
-    #     url=event.get('url_private')
-    #     token=os.environ['SLACK_TOKEN']
-    #     # requests.get(url, headers={'Authorization': 'Bearer %s' % token})
-    #     print(url)
-    if BOT_ID!=user_id:
-        text=event.get('text')
-        client.chat_postMessage(channel=channel_id,text=text)
+    #     text=event.get('text')
+    #     client.chat_postMessage(channel=channel_id,text=text)
 
 
-if __name__=="__main__":
+if __name__ == "__main__":
     app.run(debug=True)
